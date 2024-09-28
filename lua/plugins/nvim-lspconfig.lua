0f831cd39ab74e76dbd4fcc0d4cdf3a08c1524ae
Victor Lantigua <lantiguav@gmail.com>
2024-09-28 12:21:52 -0400

    fix: show hidden files in neo-tree

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,7 @@
 -- LSP keymaps
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
+  opts = function(_, opts)
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     -- change a keymap
     -- keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
@@ -11,5 +11,18 @@ return {
     -- add a keymap
     -- keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
     keys[#keys + 1] = { "H", vim.lsp.buf.hover }
+
+    -- Don't show diagnistics for .env files
+    local on_publish_diagnostics = vim.lsp.diagnostic.on_publish_diagnostics
+    opts.servers.bashls = vim.tbl_deep_extend("force", opts.servers.bashls or {}, {
+      handlers = {
+        ["textDocument/publishDiagnostics"] = function(err, res, ...)
+          local file_name = vim.fn.fnamemodify(vim.uri_to_fname(res.uri), ":t")
+          if string.match(file_name, "^%.env") == nil then
+            return on_publish_diagnostics(err, res, ...)
+          end
+        end,
+      },
+    })
   end,
 }
