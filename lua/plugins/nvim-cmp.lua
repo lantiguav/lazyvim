87a93776c06ad9258fe61c0ea38d1c8cca1eaf63
Victor Lantigua <lantiguav@gmail.com>
2024-09-28 12:21:29 -0400

    feat: disable completion on enter

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,10 +1,17 @@
 return {
   "nvim-cmp",
   opts = function(_, opts)
+    local cmp = require("cmp")
+
     table.insert(opts.sources, 1, {
       name = "copilot",
       group_index = 2,
       priority = 100,
     })
+
+    -- Disable completion on <CR>. Use <C-y> to confirm
+    opts.mapping["<CR>"] = cmp.config.disable
+    opts.mapping["<C-y>"] = cmp.mapping.confirm({ select = true })
+    cmp.setup(opts)
   end,
 }
