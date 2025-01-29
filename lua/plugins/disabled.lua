021203fd186837450ff7c3ae79aa0f769f19f00a
Victor Lantigua <lantiguav@gmail.com>
2025-01-28 20:24:28 -0400

    fix: disable plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
   -- { "rcarriga/nvim-notify", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
   -- { "folke/noice.nvim", enabled = false },
 }
