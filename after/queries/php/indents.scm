1ef93c6dade59500dbf09428ebba06ab092db691
Victor Lantigua <lantiguav@gmail.com>
2025-01-28 20:26:09 -0400

    fix: comment out things that are erroring

--- after/queries/php/indents.scm
+++ after/queries/php/indents.scm
@@ -5,6 +5,6 @@
   (return_statement
     (object_creation_expression))
   ; prevent double indent for `return function() { ... }`
-  (return_statement
-    (anonymous_function_creation_expression))
+  ; (return_statement
+  ;   (anonymous_function_creation_expression))
 ] @indent.dedent
